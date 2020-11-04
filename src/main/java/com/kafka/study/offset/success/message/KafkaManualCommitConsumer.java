d2893d46bf32b55a1325ae72207f115b15aaeeca
haoshuhu <haoshuhu@bingo-yes.com.cn>
2020-11-04 08:25:07 +0800

    connect kafka by inside and outside ip and on sasl success

--- src/main/java/com/kafka/study/offset/success/message/KafkaManualCommitConsumer.java
+++ src/main/java/com/kafka/study/offset/success/message/KafkaManualCommitConsumer.java
@@ -6,22 +6,31 @@ import java.util.Arrays;
 import java.util.List;
 import java.util.Properties;
 
+import org.apache.kafka.clients.CommonClientConfigs;
 import org.apache.kafka.clients.consumer.ConsumerRecord;
 import org.apache.kafka.clients.consumer.ConsumerRecords;
 import org.apache.kafka.clients.consumer.KafkaConsumer;
+import org.apache.kafka.common.config.SaslConfigs;
 
 public class KafkaManualCommitConsumer {
 
     public static void main(String[] args) {
         Properties props = new Properties();
-        props.put("bootstrap.servers", "192.168.66.121:9092");
-        props.put("group.id", "lovelyGroup");
+        props.put("bootstrap.servers", "103.231.146.62:39092");
+        //props.put("bootstrap.servers", "192.168.90.3:9992");
+        props.put("group.id", "report-obuid");
         props.put("enable.auto.commit", "false");
         props.put("key.deserializer", "org.apache.kafka.common.serialization.StringDeserializer");
         props.put("value.deserializer", "org.apache.kafka.common.serialization.StringDeserializer");
+
+        props.put(CommonClientConfigs.SECURITY_PROTOCOL_CONFIG, "SASL_PLAINTEXT");
+        props.put(SaslConfigs.SASL_MECHANISM, "PLAIN");
+        props.put("sasl.jaas.config",
+                "org.apache.kafka.common.security.plain.PlainLoginModule required username=\""
+                        + "admin" + "\" password=\"" + "admin" + "\";");
         @SuppressWarnings("resource")
         KafkaConsumer<String, String> consumer = new KafkaConsumer<>(props);
-        consumer.subscribe(Arrays.asList("powerTopic"));
+        consumer.subscribe(Arrays.asList("report-obuid"));
         final int minBatchSize = 200;
         List<ConsumerRecord<String, String>> list = new ArrayList<>();
         while (true) {
@@ -32,7 +41,8 @@ public class KafkaManualCommitConsumer {
             }
             if (list.size() >= minBatchSize) {
                 System.out.println("list中的缓存数据大于minBatchSize时批量进行处理");
-                consumer.commitSync();
+                //todo test code
+                //consumer.commitSync();
                 System.out.println("全部数据处理成功后手动提交");
                 list.clear();
             }
--- src/main/java/com/kafka/study/offset/success/message/KafkaMessageProducer.java
+++ src/main/java/com/kafka/study/offset/success/message/KafkaMessageProducer.java
@@ -2,15 +2,17 @@ package com.kafka.study.offset.success.message;
 
 import java.util.Properties;
 
+import org.apache.kafka.clients.CommonClientConfigs;
 import org.apache.kafka.clients.producer.KafkaProducer;
 import org.apache.kafka.clients.producer.Producer;
 import org.apache.kafka.clients.producer.ProducerRecord;
+import org.apache.kafka.common.config.SaslConfigs;
 
 public class KafkaMessageProducer {
 
     public static void main(String[] args) {
         Properties props = new Properties();
-        props.put("bootstrap.servers", "192.168.66.121:9092");
+        props.put("bootstrap.servers", "103.231.146.62:39092");
         props.put("acks", "all");
         props.put("retries", 0);
         props.put("batch.size", 16384);
@@ -19,10 +21,15 @@ public class KafkaMessageProducer {
         props.put("buffer.memory", 33554432);
         props.put("key.serializer", "org.apache.kafka.common.serialization.StringSerializer");
         props.put("value.serializer", "org.apache.kafka.common.serialization.StringSerializer");
+        props.put(CommonClientConfigs.SECURITY_PROTOCOL_CONFIG, "SASL_PLAINTEXT");
+        props.put(SaslConfigs.SASL_MECHANISM, "PLAIN");
+        props.put("sasl.jaas.config",
+                "org.apache.kafka.common.security.plain.PlainLoginModule required username=\""
+                        + "admin" + "\" password=\"" + "admin" + "\";");
 
         Producer<String, String> producer = new KafkaProducer<>(props);
-        for (int i = 0; i < 100; i++)
-            producer.send(new ProducerRecord<String, String>("powerTopic", Integer.toString(i), Integer.toString(i)));
+        for (int i = 0; i < 200; i++)
+            producer.send(new ProducerRecord<String, String>("report-obuid", Integer.toString(i), Integer.toString(i)+"aaaaa"));
 
         producer.close();
 
